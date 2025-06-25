c93bd6be20b8a51b78c71ae6ac2a2bf730808ddb
DianWeerasekara <dian@hunome.com>
2025-06-25 16:52:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Footer from './components/footer/Footer';
 import Navbars from './components/navbar/Navbar';
 import SearchBar from './components/searchbar/SearchBar';
+import BlogPosts from './components/blog-posts/BlogPosts';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className='page-container'>
         <div className='content-wrap'>
           <SearchBar />
+          <BlogPosts />
           <h1>Hello World!</h1>
         </div>
         <Footer />
