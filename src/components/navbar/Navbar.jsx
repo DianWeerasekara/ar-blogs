4d970d18adf21c4008c2d6766c61083d6afc20cc
DianWeerasekara <dian@hunome.com>
2025-07-19 19:13:47 +0300

    logout button added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,33 @@
-import {Container} from 'react-bootstrap';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Container, Nav, Navbar, Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css'; // ✅ make sure this is imported
 
 function Navbars() {
-    return(
-        <Navbar bg="primary" data-bs-theme="dark" fixed='top'>
-            <Container>
-                <Navbar.Brand as={Link} to='/'>Blogs</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link as={Link} to='/my-blogs'>My Blogs</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar bg="primary" data-bs-theme="dark" fixed="top">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Blogs</Navbar.Brand>
+
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/my-blogs">My Blogs</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+
+        {/* ✅ User Dropdown on the right */}
+        <Dropdown align="end">
+          <Dropdown.Toggle variant="secondary" id="dropdown-user" className="d-flex align-items-center">
+            <i className="bi bi-person-circle fs-4"></i>
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item as={Link} to="/profile">My Profile</Dropdown.Item>
+            <Dropdown.Item onClick={() => alert('Logged out!')}>Logout</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navbars;
